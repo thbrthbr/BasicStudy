cae83d6ead31c79c98795743a07085392e080135
thbrthbr <tarzenezrat@gmail.com>
2023-07-16 22:31:03 +0900

    문자열 체크

---
+++ 간단문제/문자열체크.js
@@ -0,0 +1,10 @@
+function solution(babbling) {
+    var answer = 0;
+    const regex = /^(aya|ye|woo|ma)+$/;
+  
+    babbling.forEach(word => {
+      if (regex.test(word)) answer++;  
+    })
+  
+    return answer;
+  }
