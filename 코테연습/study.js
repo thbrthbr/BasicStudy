5491bfa8d0f9bf6a7be34768a1be2ac9789e33b7
thbrthbr <tarzenezrat@gmail.com>
2024-01-08 16:23:50 +0900

    240108

--- 코테연습/study.js
+++ 코테연습/study.js
@@ -2067,3 +2067,19 @@ closure()
 //
 //
 //
+
+function solution(phone_book) {
+    phone_book.sort()
+    console.log(phone_book.sort())
+    for (let i = 0; i < phone_book.length - 1; i++) {
+        console.log(phone_book[i + 1].slice(0, phone_book[i].length))
+        if (
+            phone_book[i] === phone_book[i + 1].slice(0, phone_book[i].length)
+        ) {
+            return false
+        }
+    }
+    return true
+}
+
+console.log(solution(['123', '21']))
